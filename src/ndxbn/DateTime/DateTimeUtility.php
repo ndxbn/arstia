ed6a3c431a53755abe21d5bc5c43cebe6bfb81ae
sitri <wc348nka@gmail.com>
2016-12-19 15:44:18 +0900

    implement DateTimeUtility::toMutable method

--- src/ndxbn/DateTime/DateTimeUtility.php
+++ src/ndxbn/DateTime/DateTimeUtility.php
@@ -31,4 +31,27 @@ class DateTimeUtility
         }
         return $fixed;
     }
+
+    /**
+     * convert to Mutable (or nothing to do.)
+     *
+     * @param DateTimeInterface $dateTime
+     *
+     * @return DateTime
+     */
+    public function toMutable(DateTimeInterface $dateTime)
+    {
+        if ($dateTime instanceof DateTimeImmutable) {
+            $fixed = new DateTime($dateTime->format('Y-m-d H:i:s'));
+        } elseif ($dateTime instanceof DateTime) {
+            $fixed = $dateTime;
+        } else {
+            /**
+             * The instance of DateTimeInterface is either DateTime or DateTimeImmutable.
+             * @link http://jp2.php.net/manual/en/class.datetimeinterface.php
+             */
+            throw new \LogicException('incompatible PHP version.');
+        }
+        return $fixed;
+    }
 }
