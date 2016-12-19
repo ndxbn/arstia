d50b31ff6f9cd7e515dc4e426eb98e5f212f2c29
sitri <wc348nka@gmail.com>
2016-12-19 13:33:37 +0900

    implement DateRangeTest case

--- tests/ndxbn/DateTime/DateRangeTest.php
+++ tests/ndxbn/DateTime/DateRangeTest.php
@@ -4,8 +4,28 @@ namespace ndxbn\DateTime;
 
 class DateRangeTest extends \PHPUnit_Framework_TestCase
 {
-    public function testTest()
+    public function testInterfaceMethods()
     {
-        $this->markTestIncomplete();
+        $startAt = new \DateTimeImmutable('2000/12/10 12:34:56');
+        $endAt = new \DateTimeImmutable('2000/12/16 12:34:56');
+        $dateRange = new DateRange($startAt, $endAt);
+
+        $this->assertInstanceOf(\DateTimeInterface::class, $dateRange->getStart());
+        $this->assertEquals($startAt, $dateRange->getStart());
+
+        $this->assertInstanceOf(\DateTimeInterface::class, $dateRange->getEnd());
+        $this->assertEquals($endAt, $dateRange->getEnd());
+
+        $this->assertInstanceOf(\DateInterval::class, $dateRange->interval());
+        $this->assertSame(6, $dateRange->interval()->days);
+
+        // once iterator
+        $this->assertInstanceOf(\DatePeriod::class, $dateRange->toDatePeriod());
+        $counter = 0;
+        foreach ($dateRange->toDatePeriod() as $item) {
+            $this->assertInstanceOf(\DateTimeInterface::class, $item);
+            $counter++;
+        }
+        $this->assertLessThanOrEqual(1, $counter);
     }
 }
